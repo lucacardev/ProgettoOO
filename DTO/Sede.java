8261fb03ba681af80141a83f6dfb15b7ceca8f19
lucacardev <132443741+lucacardev@users.noreply.github.com>
2023-05-03 15:38:04 +0200

    Add files via upload

--- DTO/Sede.java
+++ DTO/Sede.java
@@ -1,52 +1,52 @@
 package DTO;
 
 public class Sede {
-	
-	private Integer CodS;
-	private String nome;
-	private String indirizzo;
-	private String responsabile;
-	
-	public Sede (Integer codS,String nome,String indirizzo,String responsabile) {
-		this.CodS = codS;
-		this.nome = nome;
-		this.indirizzo = indirizzo;
-		this.responsabile = responsabile;
-	}
-	
+
+    private Integer CodS;
+    private String nome;
+    private String indirizzo;
+    private String responsabile;
+
+    public Sede (Integer codS,String nome,String indirizzo,String responsabile) {
+        this.CodS = codS;
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.responsabile = responsabile;
+    }
+
 //GETTERS E SETTERS
 
-	public Integer getCodS() {
-		return CodS;
-	}
+    public Integer getCodS() {
+        return CodS;
+    }
 
-	public void setCodS(Integer codS) {
-		this.CodS = codS;
-	}
+    public void setCodS(Integer codS) {
+        this.CodS = codS;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getIndirizzo() {
-		return indirizzo;
-	}
+    public String getIndirizzo() {
+        return indirizzo;
+    }
 
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
-	}
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
 
-	public String getResponsabile() {
-		return responsabile;
-	}
+    public String getResponsabile() {
+        return responsabile;
+    }
 
-	public void setResponsabile(String responsabile) {
-		this.responsabile = responsabile;
-	}
+    public void setResponsabile(String responsabile) {
+        this.responsabile = responsabile;
+    }
 
-	
+
 }
